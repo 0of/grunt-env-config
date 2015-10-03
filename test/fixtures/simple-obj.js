e069c78678909fd73a7c13d7067d92d0456816cf
magnus <Magnusbackyard@live.com>
2015-10-03 23:21:11 +0800

    test

---
+++ test/fixtures/simple-obj.js
@@ -0,0 +1,8 @@
+module.exports = {
+    a: 10,
+    n: 5.5,
+    s: "string",
+    o: {
+        b: false
+    }
+};
---
+++ test/gruntfile.js
@@ -0,0 +1,61 @@
+var fs = require('fs'),
+    path = require('path'),
+    assert = require('assert');
+
+var enableEnvConfig = require('../index');
+
+module.exports = function (grunt) {
+
+    var simpleObjPath = path.join(__dirname, './fixtures/simple-obj.js');
+
+    grunt.initConfig({
+        templateRequire: {
+            options: {
+                a: '<%= $require.templateRequire.ref %>'
+            },
+            ref: simpleObjPath
+        },
+        envTemplateHelper: {
+            options: {
+                a: '<%= grunt.template.env("a") %>'
+            }
+        },
+        throughTemplateHelper: {
+            options: {
+                a: '<%= grunt.template.through(process.env.a) %>'
+            }
+        }
+    });
+
+    grunt.registerTask('templateRequire', 'should require successfully', function () {
+        var opts = this.options(),
+            simpleObj = require(simpleObjPath);
+
+        assert.deepEqual(simpleObj, opts.a);
+    });
+
+    grunt.registerTask('envTemplateHelper', 'should access the env variable successfully', envTest);
+
+    grunt.registerTask('throughTemplateHelper', 'should access the env variable successfully', envTest);
+
+    grunt.registerTask('test', ['templateRequire', 'envTemplateHelper', 'throughTemplateHelper']);
+
+    function envTest() {
+        var simpleObj = JSON.stringify(require(simpleObjPath));
+        var opts;
+
+        process.env.a = simpleObj;
+        try {
+            opts = this.options();
+        } catch (e) {
+            delete process.env.a;
+            throw e;
+        }
+
+        assert.equal(simpleObj, opts.a);
+        delete process.env.a;
+    }
+
+    enableEnvConfig(grunt);
+}
+
